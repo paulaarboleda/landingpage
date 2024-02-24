f0a38e0eff90ca34bd105eeef3e673c9a5d137e9
Paula <paulola0223@gmail.com>
2024-02-24 15:28:11 -0300

    Organizar tags e css

--- script.js
+++ script.js
@@ -1,6 +1,99 @@
-function validarFormario(){
-    alert ("Dados enviados!, em breve entraremos em contato!")
+function validarFormario() {
+    alert("Dados enviados!, em breve entraremos em contato!")
 }
+
+
+/*
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+
+form.addEventListener("button", (Event) => {
+    Event.preventDefault();
+    //alert("Cadastrado com sucesso!")
+})
+
+function checkInputUsername() {
+    const usernameValue = nome.valeu;
+
+    //console.log(usernameValue)
+
+if (usernameValue == "") {
+errorInput(username, "Preencha o campo")
+}
+}
+
+function errorInput (input,mensagem){
+    const formaItens = input.parentElement("a")
+    textMenssage,innerText = message;
+    formaItens.className =  "form-content error"
+}
+
+*/
+
+/*const nome = document.querySelector('#nome');
+inputnome.valeu='';
+const email = document.querySelector('#email');
+const button = document.querySelector('#button');
+
+button.addEventListener('click', () =>{
+    if (nome.valeu.length == 0){
+    alert("Campo vazio");
+}else{
+    alert("Hola!!!" + nome.valeu);
+}
+});*/
+
+/*const formulario = document.querySelector('.formulario');
+formulario.reset();*/
+
+
+
+
+
+
+/* limparForm(){
+    document.getElementById("nome").valeu = "";
+    document.getElementById("email").valeu = "";
+}*/
+
+
+
+/* form = document.getElementById("form");
+const text = document.getElementById("text");
+const email = document.getElementById("email");
+
+form.addEventListener("button", (Event) => {
+    Event.preventDefault();
+    alert("Cadastrado com sucesso!")
+})*/
+
+/*function checkInputUsername() {
+    const checkInputUsername = username.valeu;
+
+    if (username === "") {
+       errorInput(username, 'preencha o campo');
+    }
+}
+
+
+function errorInput(input, mensagem) {
+    const formaItens = input.parentElement;
+    const textMensagem = formItem.querySelecter("a")
+    textMensagem.innerText = mensagem;
+    formItem.className = "form-content error"*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -12,6 +105,12 @@ if(e.target.dataset.limpar == 'corrige'{
 
 })
 
+const fields = document.querySelectorAll("[required]")
+for( field of campos){
+    field.addEventListener("invalid" ,() => {
+        console.log("campo invalido")
+    })
+}
 
 function limparForm(){
     document.getElementById('nome').valeu = '';
